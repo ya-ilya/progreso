fb22980f1f827ec1491a68e61b0316cdf1cfb202
ya-ilya <ilya.tseplaev@gmail.com>
2023-09-09 17:50:21 +0300

    Update FullBright

---
+++ progreso-client/src/main/java/org/progreso/client/mixins/MixinLightmapTextureManager.java
@@ -0,0 +1,24 @@
+package org.progreso.client.mixins;
+
+import net.minecraft.client.render.LightmapTextureManager;
+import org.progreso.client.modules.render.FullBright;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(LightmapTextureManager.class)
+public abstract class MixinLightmapTextureManager {
+    @ModifyArgs(
+        method = "update",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/texture/NativeImage;setColor(III)V"
+        )
+    )
+    public void updateModifyArgs(Args args) {
+        if (FullBright.INSTANCE.getEnabled()) {
+            args.set(2, 0xFFFFFFFF);
+        }
+    }
+}
--- progreso-client/src/main/kotlin/org/progreso/client/modules/render/FullBright.kt
+++ progreso-client/src/main/kotlin/org/progreso/client/modules/render/FullBright.kt
@@ -1,25 +1,9 @@
 package org.progreso.client.modules.render
 
 import org.progreso.api.module.AbstractModule
-import org.progreso.client.Client.Companion.mc
-import org.progreso.client.events.misc.TickEvent
-import org.progreso.client.events.safeEventListener
 
+/**
+ * @see org.progreso.client.mixins.MixinLightmapTextureManager
+ */
 @AbstractModule.AutoRegister
-object FullBright : AbstractModule() {
-    init {
-        var oldGamma = 0.5
-
-        onEnable {
-            oldGamma = mc.options?.gamma?.value ?: 0.5
-        }
-
-        onDisable {
-            mc.options!!.gamma.value = oldGamma
-        }
-
-        safeEventListener<TickEvent> { _ ->
-            mc.options!!.gamma.value = 1.0
-        }
-    }
-}
+object FullBright : AbstractModule()
