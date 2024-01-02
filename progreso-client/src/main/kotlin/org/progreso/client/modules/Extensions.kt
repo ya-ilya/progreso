a08ac9a94acc77e14dc432c42f41a01ecc159567
ya-ilya <ilya.tseplaev@gmail.com>
2024-01-02 12:17:37 +0300

    Added Welcomer hud module, added SimpleHudModule abstract class

--- progreso-client/src/main/kotlin/org/progreso/client/modules/Extensions.kt
+++ progreso-client/src/main/kotlin/org/progreso/client/modules/Extensions.kt
@@ -7,7 +7,10 @@ import org.progreso.client.events.eventListener
 import org.progreso.client.events.render.Render2DEvent
 import org.progreso.client.gui.clickgui.HudEditor
 import org.progreso.client.gui.drawRect
+import org.progreso.client.gui.drawText
+import org.progreso.client.gui.textRenderer
 import org.progreso.client.modules.client.ClickGUI
+import java.awt.Color
 
 fun AbstractHudModule.render(block: DrawContext.() -> Unit) {
     eventListener<Render2DEvent> { event ->
@@ -16,5 +19,18 @@ fun AbstractHudModule.render(block: DrawContext.() -> Unit) {
         }
 
         block(event.context)
+    }
+}
+
+abstract class SimpleHudModule(private val text: () -> String) : AbstractHudModule() {
+    private val color by setting("Color", Color.RED)
+
+    override var width = 0; get() = textRenderer.getWidth(text()) + 2
+    override var height = 0; get() = textRenderer.fontHeight + 4
+
+    init {
+        render {
+            drawText(text(), x + 1, y + 2, color, true)
+        }
     }
 }
--- progreso-client/src/main/kotlin/org/progreso/client/modules/hud/Welcomer.kt
+++ progreso-client/src/main/kotlin/org/progreso/client/modules/hud/Welcomer.kt
@@ -1,9 +1,10 @@
 package org.progreso.client.modules.hud
 
 import org.progreso.api.module.AbstractModule
+import org.progreso.client.Client.Companion.mc
 import org.progreso.client.modules.SimpleHudModule
 
 @AbstractModule.AutoRegister
-object Watermark : SimpleHudModule({
-    "Progreso Client"
+object Welcomer : SimpleHudModule({
+    "Welcome to Progreso Client, ${mc.player.name.string}!"
 })
